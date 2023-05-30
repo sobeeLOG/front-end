c61e8bd1bf14057ea48937d4502f688cb152eabf
choiyoorim <yulim3839@naver.com>
2023-05-31 00:35:45 +0900

    feat: ✨피드 뷰 제작(소비내역 컴포넌트 활용)

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,11 +1,21 @@
+import React,{useState} from 'react';
 import styled from 'styled-components';
 import Navigator from '../components/common/Navigator';
 import Header from '../components/common/Header';
+import CalendarConsumption from '../components/calendar/CalendarConsumption';
+import { consumptionList } from '../mock-data/consumptionList';
 
 function Feed() {
+    const [feedList, setFeedList] = useState(consumptionList);
+
     return (
         <StyledFeed>
             <Header/>
+            <StyledFeedList>
+                {feedList && feedList.map((element)=>{
+                    return <CalendarConsumption info={element} key={element.chistoryID}></CalendarConsumption>
+                })}
+            </StyledFeedList>
             <Navigator category="feed" />
         </StyledFeed>
     );
@@ -14,5 +24,17 @@ function Feed() {
 export default Feed;
 
 const StyledFeed = styled.div`
+    overflow-y:scroll;
+    height: 80vh;
+    margin-top: 8rem;
+    margin-bottom: 10rem;
+    ::-webkit-scrollbar {
+        display: none;
+    }
+`
 
+const StyledFeedList = styled.div`
+    display:flex;
+    flex-direction: column;
+    gap: 1.2rem;
 `;
