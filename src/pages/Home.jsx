41533b70c85d834c740cf330a73082bee662cf8d
jaehuiLee <20201005@sungshin.ac.kr>
2023-06-06 06:00:57 +0900

    feat: ✨홈 시작 화면 뷰 제작

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import styled from 'styled-components';
-import Navigator from '../components/common/Navigator';
+import { Logo } from '../assets';
+import { Link } from 'react-router-dom';
 
 function Home() {
     return (
         <StyledHome>
-        <Navigator category="home" />
+            <StyledLogo>
+                <Logo/>
+            </StyledLogo>
+            <Link to="/Join" style={{ textDecoration: 'none', color: 'inherit' }}>
+                    <StyledStart>시작하기</StyledStart></Link>
         </StyledHome>
     );
 }
@@ -12,5 +17,31 @@ function Home() {
 export default Home;
 
 const StyledHome = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: 100vh;
+    overflow: hidden;
+`;
 
+const StyledStart = styled.div`
+    display: flex;
+    height: 4rem;
+    width: 25rem;
+    background-color: rgba(44, 84, 228, 1);
+    border-radius: 1rem;
+    margin-top: 5rem;
+    color: #FFFFFF;
+    text-align: center;
+    justify-content: center;
+    align-items: center;
+    font-size: 20px;
+`;
+
+const StyledLogo = styled(Logo)`
+    position: fixed;
+    height: 200px;
+    width: 200px;
+    top: 30%;
 `;
