0fc494f745d4dc350fd477b9497bdf411509868b
HwangHyeryeong <hrh4225@gmail.com>
2023-06-06 14:35:35 +0900

    feat: ✨친구 신청 답신 API 연결

--- src/components/friend/FriendRequestList.jsx
+++ src/components/friend/FriendRequestList.jsx
@@ -4,38 +4,73 @@ import './Friend.css'
 import { ICProfile } from '../../assets';
 
 function FriendRequestList() {
-  const [request, setRequest] = useState([]);
-
-    useEffect(() => {
-        const fetchFriendList = async () => {
-        try {
-            const userID = 1; //TODO: 로그인 한 사용자의 아이디를 가져오도록 바꾸기
-            const response = await client.get(`/friends/requests/${userID}`);
-            const requestList = response.data.data.requestList;
-            const requests = requestList.map(request => request.nickname);
-            setRequest(requests);
-        } catch (error) {
-            console.log(error);
-        }};
-
-        fetchFriendList();
-    }, []);
+  const [requests, setRequests] = useState([]);
+  const [myID, setMyID] = useState(null);
   
-  //TODO: 버튼 누르면 친구 수락/삭제 POST 작동하도록
+  useEffect(() => {
+    const fetchFriendRequestList = async () => {
+      try {
+        //TODO: 로그인 한 사용자의 아이디를 가져오도록 바꾸기
+        const inUserID = 1; 
+        setMyID(inUserID);
+
+        const response = await client.get(`/friends/requests/${myID}`);
+        const requestList = response.data.data.requestList;
+        setRequests(requestList);
+      } catch (error) {
+        console.log(error);
+      }};
+    
+      fetchFriendRequestList();
+    }, [myID]);
+
+    const handleAccept = async(receiverID, senderID) => {
+      try{
+        console.log("[친구 수락]: " + receiverID + ", " + senderID);
+        await client.patch('/friends/requests', {receiverID, senderID});
+
+        //수락한 요청은 목록에서 지우기
+        setRequests((prevRequests) =>
+          prevRequests.filter((request) => request.senderID !== senderID)
+        );
+      }catch(error){
+        console.log(error);
+      }
+    }
+
+    const handleReject = async(receiverID, senderID) => {
+      try{
+        console.log("[친구 거절]: " + receiverID + ", "+ senderID);
+        await client.post('/friends/requests/delete', {
+          user1ID: receiverID, 
+          user2ID: senderID
+        });
+
+        //수락한 요청은 목록에서 지우기
+        setRequests((prevRequests) =>
+          prevRequests.filter((request) => request.senderID !== senderID)
+        );
+      }catch(error){
+        console.log(error);
+      }
+    }
+  
+  //TODO: 따로 새로고침 하지 않아도 수락/거절 반영 내용 화면에 나오도록 바꾸기
   return (
     <div className='grid'>
         <p>친구 요청 목록</p>
         <div>
-            {request.map((requestListElem, index) => {
+            {requests.map((request, index) => {
+                const {userID, nickname} = request;
                 return (
                     <div className='request_list' key = {index}>
                     <div className='profile_img'>
                       <ICProfile/>
                     </div>
-                    <div> {requestListElem} </div>
+                    <div> {nickname} </div>
                     <div className='reply_buttons'> 
-                        <button className='button_blue'>수락</button> 
-                        <button className='button_grey'>거절</button>
+                        <button className='button_blue' onClick={() => handleAccept(myID, userID)}>수락</button> 
+                        <button className='button_grey' onClick={() => handleReject(myID, userID)}>거절</button>
                     </div> 
                     </div>
                 );
